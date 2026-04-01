b1c8e354c612388dac6b54e9bc8a31e7916736df
Matt_F03 <matthieu.frnd1@gmail.com>
2026-04-01 22:34:19 +0200

    Vraie version APP_datasets

--- APP_datasets.py
+++ APP_datasets.py
@@ -1,56 +1,56 @@
 avions = [
-    {"index": "AF342", "carburant": 18, "medical": False, "technique": False, "diplomatique_niv": 2},
-    {"index": "LH908", "carburant": 25, "medical": False, "technique": True,  "diplomatique_niv": 1},
-    {"index": "BA117", "carburant": 14, "medical": True,  "technique": False, "diplomatique_niv": 3},
-    {"index": "EK202", "carburant": 40, "medical": False, "technique": False, "diplomatique_niv": 5},
-    {"index": "AZ721", "carburant": 9,  "medical": False, "technique": False, "diplomatique_niv": 1}
+    {"id": "AF342", "fuel": 18, "medical": False, "technical_issue": False, "diplomatic_level": 2},
+    {"id": "LH908", "fuel": 25, "medical": False, "technical_issue": True,  "diplomatic_level": 1},
+    {"id": "BA117", "fuel": 14, "medical": True,  "technical_issue": False, "diplomatic_level": 3},
+    {"id": "EK202", "fuel": 40, "medical": False, "technical_issue": False, "diplomatic_level": 5},
+    {"id": "AZ721", "fuel": 9,  "medical": False, "technical_issue": False, "diplomatic_level": 1}
 ]
 
 AVIONS_INITIAL = [
-    {"index": "AF342", "carburant": 18, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 19.42},
-    {"index": "LH908", "carburant": 25, "medical": False, "technique": True,  "diplomatique_niv": 1, "temps_arrivee": 19.44},
-    {"index": "BA117", "carburant": 14, "medical": True,  "technique": False, "diplomatique_niv": 3, "temps_arrivee": 19.46},
-    {"index": "EK202", "carburant": 40, "medical": False, "technique": False, "diplomatique_niv": 5, "temps_arrivee": 19.47},
-    {"index": "IB455", "carburant": 12, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 19.49},
-    {"index": "AZ721", "carburant": 9, "medical": False, "technique": False, "diplomatique_niv": 1, "temps_arrivee": 19.50},
-    {"index": "UA331", "carburant": 22, "medical": False, "technique": False, "diplomatique_niv": 4, "temps_arrivee": 19.51},
-    {"index": "QR998", "carburant": 16, "medical": False, "technique": False, "diplomatique_niv": 5, "temps_arrivee": 19.52},
-    {"index": "TK876", "carburant": 8, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 19.53},
-    {"index": "AC410", "carburant": 35, "medical": False, "technique": False, "diplomatique_niv": 3, "temps_arrivee": 19.54},
-    {"index": "DL550", "carburant": 11, "medical": True, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 19.55},
-    {"index": "SU119", "carburant": 27, "medical": False, "technique": False, "diplomatique_niv": 1, "temps_arrivee": 19.56},
-    {"index": "SN204", "carburant": 6, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 19.57},
-    {"index": "KL330", "carburant": 19, "medical": False, "technique": False, "diplomatique_niv": 3, "temps_arrivee": 19.58},
-    {"index": "EY601", "carburant": 28, "medical": False, "technique": False, "diplomatique_niv": 4, "temps_arrivee": 19.59},
-    {"index": "AF118", "carburant": 15, "medical": False, "technique": True, "diplomatique_niv": 2, "temps_arrivee": 20.00},
-    {"index": "LH332", "carburant": 21, "medical": False, "technique": False, "diplomatique_niv": 1, "temps_arrivee": 20.01},
-    {"index": "BA450", "carburant": 10, "medical": False, "technique": False, "diplomatique_niv": 3, "temps_arrivee": 20.02},
-    {"index": "IB900", "carburant": 17, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 20.03},
-    {"index": "AZ333", "carburant": 13, "medical": False, "technique": False, "diplomatique_niv": 1, "temps_arrivee": 20.04},
-    {"index": "UA870", "carburant": 24, "medical": False, "technique": False, "diplomatique_niv": 4, "temps_arrivee": 20.05},
-    {"index": "QR555", "carburant": 7, "medical": False, "technique": False, "diplomatique_niv": 5, "temps_arrivee": 20.06},
-    {"index": "TK221", "carburant": 20, "medical": False, "technique": False, "diplomatique_niv": 2, "temps_arrivee": 20.07},
-    {"index": "AC990", "carburant": 5, "medical": False, "technique": False, "diplomatique_niv": 3, "temps_arrivee": 20.08},
+    {"id": "AF342", "fuel": 18, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 19.42},
+    {"id": "LH908", "fuel": 25, "medical": False, "technical_issue": True,  "diplomatic_level": 1, "arrival_time": 19.44},
+    {"id": "BA117", "fuel": 14, "medical": True,  "technical_issue": False, "diplomatic_level": 3, "arrival_time": 19.46},
+    {"id": "EK202", "fuel": 40, "medical": False, "technical_issue": False, "diplomatic_level": 5, "arrival_time": 19.47},
+    {"id": "IB455", "fuel": 12, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 19.49},
+    {"id": "AZ721", "fuel": 9, "medical": False, "technical_issue": False, "diplomatic_level": 1, "arrival_time": 19.50},
+    {"id": "UA331", "fuel": 22, "medical": False, "technical_issue": False, "diplomatic_level": 4, "arrival_time": 19.51},
+    {"id": "QR998", "fuel": 16, "medical": False, "technical_issue": False, "diplomatic_level": 5, "arrival_time": 19.52},
+    {"id": "TK876", "fuel": 8, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 19.53},
+    {"id": "AC410", "fuel": 35, "medical": False, "technical_issue": False, "diplomatic_level": 3, "arrival_time": 19.54},
+    {"id": "DL550", "fuel": 11, "medical": True, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 19.55},
+    {"id": "SU119", "fuel": 27, "medical": False, "technical_issue": False, "diplomatic_level": 1, "arrival_time": 19.56},
+    {"id": "SN204", "fuel": 6, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 19.57},
+    {"id": "KL330", "fuel": 19, "medical": False, "technical_issue": False, "diplomatic_level": 3, "arrival_time": 19.58},
+    {"id": "EY601", "fuel": 28, "medical": False, "technical_issue": False, "diplomatic_level": 4, "arrival_time": 19.59},
+    {"id": "AF118", "fuel": 15, "medical": False, "technical_issue": True, "diplomatic_level": 2, "arrival_time": 20.00},
+    {"id": "LH332", "fuel": 21, "medical": False, "technical_issue": False, "diplomatic_level": 1, "arrival_time": 20.01},
+    {"id": "BA450", "fuel": 10, "medical": False, "technical_issue": False, "diplomatic_level": 3, "arrival_time": 20.02},
+    {"id": "IB900", "fuel": 17, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 20.03},
+    {"id": "AZ333", "fuel": 13, "medical": False, "technical_issue": False, "diplomatic_level": 1, "arrival_time": 20.04},
+    {"id": "UA870", "fuel": 24, "medical": False, "technical_issue": False, "diplomatic_level": 4, "arrival_time": 20.05},
+    {"id": "QR555", "fuel": 7, "medical": False, "technical_issue": False, "diplomatic_level": 5, "arrival_time": 20.06},
+    {"id": "TK221", "fuel": 20, "medical": False, "technical_issue": False, "diplomatic_level": 2, "arrival_time": 20.07},
+    {"id": "AC990", "fuel": 5, "medical": False, "technical_issue": False, "diplomatic_level": 3, "arrival_time": 20.08},
 ]
 
 
 avions_diplomatic_50 = [
-    {"index": f"DP{i:02}",
-     "carburant": 10 + (i % 30),
+    {"id": f"DP{i:02}",
+     "fuel": 10 + (i % 30),
      "medical": False,
-     "technique": False,
-     "diplomatique_niv": 4 + (i % 2),
-     "temps_arrivee": 19.40 + i * 0.01}
+     "technical_issue": False,
+     "diplomatic_level": 4 + (i % 2),
+     "arrival_time": 19.40 + i * 0.01}
     for i in range(50)
 ]
 
 avions_diplomatic_50 = [
-    {"index": f"DP{i:02}",
-     "carburant": 10 + (i % 30),
+    {"id": f"DP{i:02}",
+     "fuel": 10 + (i % 30),
      "medical": False,
-     "technique": False,
-     "diplomatique_niv": 4 + (i % 2),
-     "temps_arrivee": 19.40 + i * 0.01}
+     "technical_issue": False,
+     "diplomatic_level": 4 + (i % 2),
+     "arrival_time": 19.40 + i * 0.01}
     for i in range(50)
 ]
 
@@ -60,10 +60,10 @@ avions_chaos_100 = []
 
 for i in range(100):
     avions_chaos_100.append({
-        "index": f"CH{i:03}",
-        "carburant": random.randint(5, 60),
+        "id": f"CH{i:03}",
+        "fuel": random.randint(5, 60),
         "medical": random.random() < 0.2,
-        "technique": random.random() < 0.15,
-        "diplomatique_niv": random.randint(1, 5),
-        "temps_arrivee": 19.40 + i * 0.005
+        "technical_issue": random.random() < 0.15,
+        "diplomatic_level": random.randint(1, 5),
+        "arrival_time": 19.40 + i * 0.005
     })
